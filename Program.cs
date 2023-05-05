0f8eda67e583b9513d17040a3559e138b97767f2
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-05-05 10:17:00 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Module14;
+
+internal class Program
+{
+    const int ItemsPerPage = 2;
+
+    static readonly List<Contact> phoneBook = new List<Contact>();
+        
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+        phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+        phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+        phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+        Run();
+    }
+
+    static void Run()
+    {
+        var sortedPhoneBook = phoneBook.OrderBy(contact => contact.Name).ThenBy(contact => contact.LastName);
+
+        bool run = true;
+        int pageCount = phoneBook.Count < ItemsPerPage ? 1 : phoneBook.Count / 2;
+        while (run)
+        {
+            Console.Write("Enter page number or \"e\" for terminate program: ");
+            string? command = Console.ReadLine()?.ToLower();
+            if (command != "e")
+            {
+                Console.Clear();
+                int page = default;
+                if (int.TryParse(command, out page) && page > 0 && page <= pageCount)
+                {
+                    var pageView = sortedPhoneBook.Skip(ItemsPerPage * (page - 1)).Take(ItemsPerPage);
+                    Console.WriteLine($"Page {page} of {pageCount}");
+                    Console.WriteLine("-------------------------------");
+                    foreach (var item in pageView)
+                    {
+                        Console.WriteLine($"{item.Name} {item.LastName} {item.PhoneNumber} {item.Email}");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Page Number.");
+                }
+            }
+            else
+            {
+                run = false;
+            }
+        }
+    }
+}
